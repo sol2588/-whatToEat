4bf0fade87b2b001d374e4e18234b9a3538dd8aa
sol2588 <dsk2588@gmail.com>
2024-11-25 21:03:17 +0900

    Refactor: SSEHandler(msw)

--- src/mocks/sseHandler.ts
+++ src/mocks/sseHandler.ts
@@ -1,7 +1,14 @@
 import { http, HttpResponse } from 'msw';
 
 export const sseHandler = [
-    http.get(`${import.meta.env.VITE_BASE_URL}/notify`, async () => {
+    http.get(`${import.meta.env.VITE_BASE_URL}/notify`, async ({ request }) => {
+        const token = request.headers.get('access-token');
+        if (!token) {
+            return HttpResponse.json({
+                code: 'Bad Request',
+                message: 'Unauthorized',
+            });
+        }
         const stream = new ReadableStream({
             start(controller) {
                 const encoder = new TextEncoder();
@@ -12,11 +19,23 @@ export const sseHandler = [
                 };
 
                 const intervalId = setInterval(() => {
-                    sendEvent('notification', '새 댓글이 달렸습니다.');
+                    sendEvent(
+                        'notification',
+                        JSON.stringify({
+                            recipeName: '오트밀',
+                            message: '새로운 메시지가 등록되었습니다. ',
+                            rating: 5,
+                            commentUser: 'NEW',
+                            recipeId: 1,
+                        }),
+                    );
                 }, 5000);
-                console.log(intervalId);
 
-                controller.close();
+                request.signal.addEventListener('abort', () => {
+                    console.log('Connection aborted');
+                    clearInterval(intervalId);
+                    controller.close();
+                });
             },
         });
 
@@ -24,7 +43,7 @@ export const sseHandler = [
             headers: {
                 'Content-Type': 'text/event-stream',
                 'Cache-Control': 'no-cache',
-                Conntection: 'keep-alive',
+                Connection: 'keep-alive',
             },
         });
     }),
