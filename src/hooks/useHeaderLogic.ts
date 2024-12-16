d986276de7eb5e6de8049bb7f579ffb57d689ceb
sol2588 <dsk2588@gmail.com>
2024-12-17 06:35:56 +0900

    Fix: useHeaderLogic - delete isLogin

--- src/hooks/useHeaderLogic.ts
+++ src/hooks/useHeaderLogic.ts
@@ -1,12 +1,8 @@
 import { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-import { RootState } from '../redux/store/store';
 
-// 리팩토링 - 클릭 메뉴 이름 props로 전달받아 menuItems와 비교
 export default function useHeaderLogic() {
     const [isActive, setIsActive] = useState<string>('');
-    const isLogin = useSelector((state: RootState) => state.user.value.isLoggedIn);
     const location = useLocation();
 
     useEffect(() => {
